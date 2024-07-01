640092ed2c051da270e89eecb4500de44fa8062d
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-01 16:35:50 +0300

    Feat: Add task "1. MongoDB utils"
    Inside the folder utils, create a file db.js that contains the class DBClient.
    
    DBClient should have:
    
    the constructor that creates a client to MongoDB:
    host: from the environment variable DB_HOST or default: localhost
    port: from the environment variable DB_PORT or default: 27017
    database: from the environment variable DB_DATABASE or default: files_manager
    a function isAlive that returns true when the connection to MongoDB is a success otherwise, false
    an asynchronous function nbUsers that returns the number of documents in the collection users
    an asynchronous function nbFiles that returns the number of documents in the collection files
    After the class definition, create and export an instance of DBClient called dbClient.
    
    ```js
    bob@dylan:~$ cat main.js
    import dbClient from './utils/db';
    
    const waitConnection = () => {
        return new Promise((resolve, reject) => {
            let i = 0;
            const repeatFct = async () => {
                await setTimeout(() => {
                    i += 1;
                    if (i >= 10) {
                        reject()
                    }
                    else if(!dbClient.isAlive()) {
                        repeatFct()
                    }
                    else {
                        resolve()
                    }
                }, 1000);
            };
            repeatFct();
        })
    };
    
    (async () => {
        console.log(dbClient.isAlive());
        await waitConnection();
        console.log(dbClient.isAlive());
        console.log(await dbClient.nbUsers());
        console.log(await dbClient.nbFiles());
    })();
    
    bob@dylan:~$ npm run dev main.js
    false
    true
    4
    30
    bob@dylan:~$
    ```
    
    File:
    - utils/db.js

---
+++ utils/db.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb');
+const process = require('process');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+  constructor() {
+    this.client = null
+    this.connect()
+  }
+
+  async connect() {
+    try {
+        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.client = client.db(DB_DATABASE);
+        this.usercollection = this.client.collection('users');
+        this.filecollection = this.client.collection('files');
+      } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+      }
+  }
+
+  isAlive() {
+    return this.client !== null;
+  }
+
+  async nbUsers() {
+    if (!this.usercollection) return 0;
+    return this.usercollection.countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.filecollection) return 0;
+    return this.filecollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
